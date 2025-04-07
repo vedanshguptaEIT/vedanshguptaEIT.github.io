f94f1eadb5a9958a34bd56d664016c372b5566c0
Ved <vedanshgupta2803@gmail.com>
2025-04-08 01:38:28 +0530

    Added CNAME for custom domain

---
+++ src/content_option.js
@@ -0,0 +1,156 @@
+const logotext = "Ved";
+const meta = {
+    title: "Vedansh Gupta",
+    description: "I’m Vedansh Gupta  Full stack devloper,currently working in Delhi",
+};
+
+const introdata = {
+    title: "I’m Vedansh Gupta",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I'm just trying.......",
+    },
+    description: "I'm a passionate front-end developer crafting modern and cosmic web experiences. Always learning, always building.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "a bit about my self",
+    aboutme: "I'm a passionate Tech & Coding enthusiast and a Front-end developer. I'm constantly exploring the exciting world of AI while delving deeper into my field. I thrive on managing Human Resources and creating meaningful connections between organizations. Sharing my work, thoughts, and experiences here fills me with absolute joy!.",
+};
+const worktimeline = [{
+    jobtitle: "Tech HR and Co-lead",
+    where: "GDSC",
+    date: "2024",
+},
+{
+    jobtitle: "Website Developer",
+    where: "EIT, Faridabad",
+    date: "2024",
+},
+{
+    jobtitle: "Hackathon Organiser",
+    where: "EIT, Faridabad",
+    date: "2024",
+},
+];
+
+const skills = [{
+    name: "React",
+    value: 90,
+},
+{
+    name: "Tailwind CSS",
+    value: 85,
+},
+{
+    name: "Javascript",
+    value: 80,
+},
+
+];
+
+const services = [{
+    title: "UI & UX Design",
+    description: " A creative UI/UX designer blending aesthetics with user-centric design. Turning ideas into intuitive interfaces.",
+},
+{
+    title: "Website Development",
+    description: "I build responsive and modern websites with seamless user experiences. Crafting pixel-perfect websites that don’t just look good — they work beautifully.",
+},
+
+];
+
+const dataportfolio = [{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "Echiesta.com",
+    link: "https://github.com/Thevedanshgupta/echiesta.com",
+},
+{
+    img: "https://picsum.photos/400/800/?grayscale",
+    description: "Moons.com",
+    link: "https://github.com/Thevedanshgupta/moons",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "ImageX",
+    link: "https://github.com/Thevedanshgupta/imagex",
+},
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    description: "ITSolutionWebsite",
+    link: "https://github.com/Thevedanshgupta/itsolutionwebsite",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/550/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "studentvedansh@gmail.com",
+    YOUR_FONE: "+91-8070101188",
+    description: "Have an idea, a project, or just want to say hello? I’m always excited to connect with passionate people. Whether you're looking for a fresh UI/UX design, a responsive website, or a creative mind to collaborate with — I’m here for it. Let’s turn your vision into reality. Feel free to drop a message!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_hj8fol7",
+    YOUR_TEMPLATE_ID: "template_c0o4idh",
+    YOUR_USER_ID: "YpPUT8fG97VlQtd-Q",
+};
+
+const socialprofils = {
+    github: "https://github.com/Thevedanshgupta",
+    instagram: "https://instagram.com/vedanshguptaa",
+    linkedin: "https://www.linkedin.com/in/thevedanshgupta/",
+    
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
